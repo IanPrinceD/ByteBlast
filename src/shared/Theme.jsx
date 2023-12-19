1d8c3ea6dbc3b87eb8e0f65881ca36a932973872
IanPrinceD <princedumanhug@gmail.com>
2023-12-19 19:50:54 +0800

    set up theme for global customization

---
+++ src/shared/Theme.jsx
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#DB4444',
+    },
+    secondary: {
+      main: '#FFF',
+    },
+    accent: {
+      main: '#000',
+    },
+  },
+});
+
+export default (theme);
